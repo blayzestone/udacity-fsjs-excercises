d70ed7250ec576dc8c050b10a49515e59ab17298
Blayze <blayzestone@protonmail.com>
2021-04-03 10:58:49 -0700

    'convert-to-typescript-1' excercise solutions

--- convert-to-typescript-1/src/index.ts
+++ convert-to-typescript-1/src/index.ts
@@ -6,7 +6,7 @@ const numArr = [3, 4, 5, 6];
 const wordArr = ['cat', 'dog', 'rabbit', 'bird'];
 const arrSum = arrays.addArr(numArr);
 const mixArr = arrays.concatArr(numArr, wordArr);
-const myNum = '15' % 2;
+const myNum = ('15' as unknown) as number % 2;
 
 // results of function calls
 console.log(arrays.cut3(mixArr));
--- convert-to-typescript-1/src/utilities/arrays.ts
+++ convert-to-typescript-1/src/utilities/arrays.ts
@@ -1,12 +1,12 @@
 // Concatenate two arrays
 
-const concatArr = (arr1, arr2) => {
+const concatArr = (arr1: (number | string)[], arr2: (number | string)[]) => {
   return [...arr1, ...arr2];
 };
 
 // Add numbers in an array
 
-const addArr = (arr) => {
+const addArr = (arr: number[]): number => {
   let total = 0;
   arr.forEach((x) => {
     total += x;
@@ -15,18 +15,18 @@ const addArr = (arr) => {
 };
 
 // Find the largest number in an array
-const lgNum = (arr) => {
-  let largest = 0;
+const lgNum = (arr: (number | string)[]): number => {
+  let largest = 0 as number;
   arr.forEach((x) => {
     if (x > largest) {
-      largest = x;
+      largest = x as number;
     }
   });
   return largest;
 };
 
 // Remove the 3rd item from an array
-const cut3 = (arr) => {
+const cut3 = (arr:[]) => {
   arr.splice(2, 1);
   return arr;
 };
